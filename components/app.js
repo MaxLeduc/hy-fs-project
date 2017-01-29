e763dafacb845b1b493b43f605b7c9d92d40b6d3
MaxLeduc <leduc.maximep@gmail.com>
2017-01-29 16:31:28 -0500

    setting initial values in inital state

--- components/app.js
+++ components/app.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 var App = React.createClass({
-	render: function () {
-		return <h1>hello</h1>
-	}
+  getInitialState: function () {
+    return {
+      ballots: [
+        {
+          title: 'Let\'s go out!',
+          description: 'We are going out like crazyyy people this Monday night, in Oshawa.',
+          options: [
+            {
+              title: 'Option1 is this',
+              votes: 0
+            },
+            {
+              title: 'Option1 is this',
+              votes: 3
+            },
+            {
+              title: 'Option1 is this',
+              votes: 2
+            }
+          ]
+        }
+      ]
+    }
+  },
+
+  render: function () {
+    return <h1>hello from the other siiiiiiiiiiiiiiiide</h1>
+  }
 })
 
 export default App;
