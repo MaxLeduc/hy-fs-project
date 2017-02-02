eee803e26759b01467f822f07cf5b35e221e36f5
Joy Liu <liukang.joy@gmail.com>
2017-02-02 18:05:47 -0500

    added voting function, changed to ES6 syntax

---
+++ components/results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Result extends React.Component {
+    render() {
+        return (
+            <div data-key={ this.props.keyd } className="results">
+                <button className="result1" onClick={ () => this.props.calculateVotes() }>{ this.props.option }</button>
+                <span className="voteCount">{ this.props.votes }</span>
+            </div>
+        )
+    }
+}
+
+export default Result;
