53b63bcbddb6a9094c3684b8a0a3eb0a9c67f3fd
MaxLeduc <leduc.maximep@gmail.com>
2017-02-05 15:32:32 -0500

    working form

--- components/app.js
+++ components/app.js
@@ -39,31 +39,70 @@ class App extends React.Component{
 }
 
 class Form extends React.Component {
-
-  constructor(props) {
-    super(props)
-    let formValue = createValue({
-      value: props.value,
-      onChange: this.onChange.bind(this)
-    })
-    this.state = {formValue}
+  constructor() {
+    super();
+    this.state = {
+      formObject: {
+        title: '',
+        description: '',
+        options: [
+          {'description': 'this is an option'},
+          {'description': 'another option'}
+        ],
+        newOption: ''
+      }
+    };
+    this.updateFromObject = this.updateFromObject.bind(this);
+    this.updateOptionsValues = this.updateOptionsValues.bind(this);
+    this.addAnOption = this.addAnOption.bind(this);
   }
 
   render() {
-    return (
-      <Fieldset formValue={this.state.formValue}>
-        <Field select="Title" label="Title" />
-        <Field select="textarea" label="textarea" />
-        <Field select="Option1" label="Option 1" />
-        <Field select="Option3" label="Option 2" />
-        <Field select="Option3" label="Option 3" />
-        <input type="submit" />
-      </Fieldset>
-    )
+    return <form>
+      <fieldset>
+        <label>Title</label>
+        <input type="textarea" name="title" onChange={ (evt) => this.updateFromObject(evt) } value={ this.state.formObject.title } />
+      </fieldset>
+      <fieldset>
+        <label>Description</label>
+        <input type="textarea" name="description" onChange={ (evt) => this.updateFromObject(evt) } value={ this.state.formObject.description } />
+      </fieldset>
+      { this.state.formObject.options.map((option, i) => (
+        <fieldset key={i}>
+          <label>Option {i + 1}</label>
+          <input type="textarea" name={i} onChange={ (evt) => this.updateOptionsValues(evt) } value={option.description} />
+        </fieldset>
+        ))
+      }
+      <fieldset>
+        <input type="textarea" name="newOption" onChange={ (evt) => this.updateFromObject(evt) } value={ this.state.formObject.newOption }/>
+        <button onClick={(evt) => this.addAnOption(evt)}>Add an option</button>
+      </fieldset>
+      <input type="submit" />
+    </form>
   }
 
-  onChange(formValue) {
-    this.setState({formValue})
+  updateFromObject(evt) {
+    let input = evt.target;
+    let formValues = Object.assign(this.state.formObject);
+    formValues[input.name] = input.value;
+    this.setState({formObject: formValues});
+  }
+
+  updateOptionsValues(evt) {
+    let input = evt.target;
+    let formValues = Object.assign(this.state.formObject);
+    formValues.options[input.name].description = input.value;
+    this.setState({formObject: formValues});
+  }
+
+  addAnOption(evt) {
+    evt.preventDefault();
+    let inputValue = evt.target.value;
+    let formValues = Object.assign(this.state.formObject);
+    formValues.options.push({'description': formValues.newOption});
+    formValues.newOption = '';
+    this.setState({formObject: formValues});
   }
 };
 
