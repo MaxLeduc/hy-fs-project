e83b88531976475f7a6495e7548c887a603c1419
joy liu <joy@joykliu.io>
2017-02-15 16:05:33 -0500

    added signin component

---
+++ components/signIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import firebase from 'firebase';
+
+class SignIn extends React.Component{
+    render() {
+        return <div>sign in component</div>
+    }
+}
+
+export default SignIn;
