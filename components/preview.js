8016785c064cbff7421ee45f114f682c8183584a
MaxLeduc <leduc.maximep@gmail.com>
2017-02-25 18:48:33 -0500

    updated voting function

--- components/preview.js
+++ components/preview.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import Heading from './heading';
 
 import firebase from 'firebase';
 
 export default class Preview extends React.Component{
     constructor() {
         super();
-        this.getCurrentUser=this.getCurrentUser.bind(this);
     }
 
     render() {
         return <div key= { this.props.datakey }>
-            <span onClick={ this.getCurrentUser }>{ this.props.option }</span>
+            <span onClick={ (evt) => {this.props.setTheUser(this.props.optionKey)} }>{ this.props.option }</span>
             <span> { this.props.votes ? this.props.votes.length - 1 : null}</span>
         </div>
-    }
-
-    getCurrentUser() {
-        let users = [0];
-        const user = firebase.auth().currentUser.uid;
-        users.push(user);
-        this.props.onVote(users);
     }
 }
--- components/renderingForm.js
+++ components/renderingForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 import firebase from 'firebase';
 
 import Form from './form';
-import Heading from './heading';
+import FormHeading from './formHeading';
 import Preview from './preview';
 import Result from './results';
 
@@ -23,55 +23,92 @@ export default class RenderingForm extends React.Component {
                         'users': [0]
                     }
                 ]
-            }
+            },
+            currentUser: ''
         };
-        this.updateForm = this.updateForm.bind(this);
-        this.onVote = this.onVote.bind(this)
-    }
-
-    updateForm(formValues) {
-        this.setState({formObject: formValues})
-    }
-
-    onVote(i, users) {
-        let options = this.state.formObject.options;
-        options[i].users = users;
-        const formValues = this.state.formObject;
-        formValues.options = options;
-
-        var windowURL = window.location.href
-        windowURL = windowURL.split('=').pop()
-
-        const currentIssue = firebase.database().ref('formValues/' + windowURL);
-
-        currentIssue.set({
-            title: this.state.formObject.title,
-            desc: this.state.formObject.desc,
-            options: options
-        })
-    }
-
-    componentDidMount() {
-      var windowURL = window.location.href
-      windowURL = windowURL.split('=').pop()
-      const allOFirebase = firebase.database().ref('formValues');
-      allOFirebase.on('value', (snapshot) => {
-        const formValues = snapshot.val();
-        formValues[windowURL] ? this.setState({formObject: formValues[windowURL]}) : '';
-        console.log(formValues[windowURL].options)
-      });
+        this.onVote = this.onVote.bind(this);
+        this.setCurrentUser = this.setCurrentUser.bind(this);
+        this.getCurrentVoteURL = this.getCurrentVoteURL.bind(this);
+        this.verifyIfUserHasVoted = this.verifyIfUserHasVoted.bind(this);
+        this.pushCurrentUserInLocalOptions = this.pushCurrentUserInLocalOptions.bind(this);
+        this.updateFirebaseUsers = this.updateFirebaseUsers.bind(this);
     }
 
     render() {
         return <div>
-            <Heading title={ this.state.formObject.title }
+            <FormHeading title={ this.state.formObject.title }
                     desc={ this.state.formObject.desc }/>
             {this.state.formObject.options.map((option, i) => (
                 <Preview option={ option.desc }
                         key={ i }
+                        optionKey={i}
                         votes={ option.users ? option.users : null }
-                        onVote={ (users) => this.onVote(i, users) }/>
+                        setTheUser={(key) => this.setCurrentUser (key)}
+                        />
             ))}
         </div>
+    }
+
+    componentDidMount() {
+      var windowURL = this.getCurrentVoteURL()
+      const allOFirebase = firebase.database().ref('formValues');
+      allOFirebase.on('value', (snapshot) => {
+        const formValues = snapshot.val();
+        formValues[windowURL] ? this.setState({formObject: formValues[windowURL]}) : '';
+      });
+    }
+
+    setCurrentUser(key) {
+        const user = firebase.auth().currentUser.uid;
+        this.setState({currentUser: user})
+        setTimeout(function() { this.onVote(key); }.bind(this));
+    }
+
+    onVote(key) {
+        var didUserVote = this.verifyIfUserHasVoted();
+        if (didUserVote) {
+            console.log('you have already voted!')
+        } else {
+            this.pushCurrentUserInLocalOptions(key);
+            this.updateFirebaseUsers()
+        }
+    }
+
+    getCurrentVoteURL() {
+        var windowURL = window.location.href;
+        windowURL = windowURL.split('=').pop();
+        return windowURL;
+    }
+
+    verifyIfUserHasVoted() {
+        let formObject = Object.assign(this.state.formObject)
+        let users = formObject.options.map(option => option.users);
+        let flattenedUsers = [].concat.apply([], users);
+        const didUserVote = flattenedUsers.indexOf(this.state.currentUser) > -1;
+        return didUserVote
+    }
+
+    pushCurrentUserInLocalOptions(key) {
+        let formObject = Object.assign(this.state.formObject);
+        formObject.options[key].users.push(this.state.currentUser);
+    }
+
+    updateFirebaseUsers() {
+        let currentIssueKey = this.getCurrentVoteURL();
+        console.log(currentIssueKey)
+        let formObject = Object.assign(this.state.formObject);
+        console.log(formObject)
+        const database = firebase.database().ref('formValues/' + currentIssueKey);
+        database.set({
+            title: formObject.title,
+            desc: formObject.desc,
+            options: formObject.options
+        })
+        .then(function () {
+            console.log('sync successful');
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
     }
 }
